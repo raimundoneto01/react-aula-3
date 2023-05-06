53f0db9d01065b91fc5a56d0ec4cd4338ded5c7f
angelolustosa <angelolusosa@gmail.com>
2023-05-05 21:16:34 -0300

    aula2

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './index.css'
+
+const Header = () => {
+    let logado = false;
+    let nome = "Gleidson"
+
+    return(
+        <>
+            <header>
+                <nav>
+                    <div className='logo'>
+                        LOGO
+                    </div>
+                    { logado &&
+                        <div className='perfil'>
+                            <h5>Bem vindo(a) {nome}</h5>
+                        </div>
+                    }
+                    <ul>
+                        <li>
+                            <a href="#!">Home</a>
+                        </li>
+                        <li>
+                            <a href="#!">Sobre</a>
+                        </li>
+                        <li>
+                            <a href="#!">Portifólio</a>
+                        </li>
+                        <li>
+                            <a href="#!">Contatos</a>
+                        </li>
+                    </ul>
+                </nav>
+                <h6>&copy; Todos os direitos reservados.</h6>
+            </header>
+        </>
+    );
+}
+
+export default Header;
